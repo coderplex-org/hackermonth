f32b2b5bdfdeed52958830dd5a99f788ae15f9c4
duttakapil <kapildutta28@gmail.com>
2016-11-24 23:15:25 +0530

    Second version of full project

---
+++ meetup02/js/timer.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+	$('.js-timer').countdown('2016/12/10 14:00:00',
+		function(event) {
+			var $this = $(this).html(event.strftime('' +
+				'<span>%d</span> days ' +
+				'<span>%H</span> hr ' +
+				'<span>%M</span> min ' +
+				'<span>%S</span> sec'));
+		});
+});
